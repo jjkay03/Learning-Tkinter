3722e1041ea9df4b4731919b8090865ec1721ac0
jjkay03 <61110962+jjkay03@users.noreply.github.com>
2024-01-04 17:04:19 +0100

    Updated Tk learn 13

--- Learning/Layout/Tk learn 13 (Pack).py
+++ Learning/Layout/Tk learn 13 (Pack).py
@@ -8,22 +8,22 @@ import ttkbootstrap as ttk
 
 # ----------------------------------- Setup ---------------------------------- #
 window = ttk.Window()
-window.title("Layout")
+window.title("Pack")
 window.geometry("400x600")
 
 
 # ---------------------------------- Widgets --------------------------------- #
 label1 = ttk.Label(window, text="First label", background="red")
 label2 = ttk.Label(window, text="Label 2", background="blue")
-label3 = ttk.Label(window, text="Las label", background="green")
+label3 = ttk.Label(window, text="Las of the label", background="green")
 button = ttk.Button(window, text="Button")
 
 
 # ---------------------------------- Layout ---------------------------------- #
-label1.pack(side="bottom")
-label2.pack(side="bottom")
-label3.pack(side="bottom")
-button.pack(side="bottom")
+label1.pack(side="top", expand=True, fill="both", padx=5, pady=5)
+label2.pack(side="left", expand=True, fill="both")
+label3.pack(side="top", expand=True, fill="both")
+button.pack(side="top", expand=True, fill="both")
 
 
 # ------------------------------------ Run ----------------------------------- #
